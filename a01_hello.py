52b59d37a92e55628171f886d11ab0e2c2c62746
sadas-jazzy <raul.martinst@gmail.com>
2024-09-25 08:30:07 -0300

    sim

--- a01_hello.py
+++ a01_hello.py
@@ -7,3 +7,13 @@ print(a+b)
 texto= 'essá é uma frase'
 
 print(texto + "9")
+
+a = 15.9 + 3 
+
+aumento = True
+print(aumento)
+
+media = 7.3
+novaMedia = 7,9
+print(media)
+print(novaMedia)
---
+++ a02-input.py
@@ -0,0 +1,13 @@
+nome2 = input("Digite seu nome ")
+
+print(nome2)
+
+n1=input("Digite um número inteiro ")
+n2=input("Digite outro ")
+n1=int (n1)
+n2=int (n2)
+print(n1+n2)
+
+sobreNome = input("Digite seu sobrenome: ")
+idade = input ("Digite sua idade: ")
+print(f"Seu sobreNome é {nome2} {sobreNome} e sua idade é {idade}")
